cb3aa792f54143613a392c4ba937174b7aaa0370
khangthk <khangthk@gmail.com>
2022-09-07 00:12:28 +0700

    [base] Update global define

--- DicomBase/global.h
+++ DicomBase/global.h
@@ -3,7 +3,7 @@
 #include <QtCore/qglobal.h>
 
 #ifndef BUILD_STATIC
-# if defined(DICOMBASE_LIB)
+# if defined(DICOMBASE_LIBRARY)
 #  define DICOMBASE_EXPORT Q_DECL_EXPORT
 # else
 #  define DICOMBASE_EXPORT Q_DECL_IMPORT
