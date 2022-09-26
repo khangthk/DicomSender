dc51bf237a4121cde91a22de97faa31c6abe62b8
khangthk <khangthk@gmail.com>
2022-09-26 17:53:36 +0700

    [base] Update echo function of dcmtk

--- DicomBase/echodcmtk.cpp
+++ DicomBase/echodcmtk.cpp
@@ -10,59 +10,11 @@
 #include <QString>
 #include <QTextStream>
 
-const char *transferSyntaxes[] = {
-      UID_LittleEndianImplicitTransferSyntax, /* default xfer syntax first */
-      UID_LittleEndianExplicitTransferSyntax,
-      UID_BigEndianExplicitTransferSyntax,
-      UID_JPEGProcess1TransferSyntax,
-      UID_JPEGProcess2_4TransferSyntax,
-      UID_JPEGProcess3_5TransferSyntax,
-      UID_JPEGProcess6_8TransferSyntax,
-      UID_JPEGProcess7_9TransferSyntax,
-      UID_JPEGProcess10_12TransferSyntax,
-      UID_JPEGProcess11_13TransferSyntax,
-      UID_JPEGProcess14TransferSyntax,
-      UID_JPEGProcess15TransferSyntax,
-      UID_JPEGProcess16_18TransferSyntax,
-      UID_JPEGProcess17_19TransferSyntax,
-      UID_JPEGProcess20_22TransferSyntax,
-      UID_JPEGProcess21_23TransferSyntax,
-      UID_JPEGProcess24_26TransferSyntax,
-      UID_JPEGProcess25_27TransferSyntax,
-      UID_JPEGProcess28TransferSyntax,
-      UID_JPEGProcess29TransferSyntax,
-      UID_JPEGProcess14SV1TransferSyntax,
-      UID_RLELosslessTransferSyntax,
-      UID_DeflatedExplicitVRLittleEndianTransferSyntax,
-      UID_JPEGLSLosslessTransferSyntax,
-      UID_JPEGLSLossyTransferSyntax,
-      UID_JPEG2000LosslessOnlyTransferSyntax,
-      UID_JPEG2000TransferSyntax,
-      UID_JPEG2000Part2MulticomponentImageCompressionLosslessOnlyTransferSyntax,
-      UID_JPEG2000Part2MulticomponentImageCompressionTransferSyntax,
-      UID_MPEG2MainProfileAtMainLevelTransferSyntax,
-      UID_MPEG2MainProfileAtHighLevelTransferSyntax,
-      UID_MPEG4HighProfileLevel4_1TransferSyntax,
-      UID_MPEG4BDcompatibleHighProfileLevel4_1TransferSyntax,
-      UID_MPEG4HighProfileLevel4_2_For2DVideoTransferSyntax,
-      UID_MPEG4HighProfileLevel4_2_For3DVideoTransferSyntax,
-      UID_MPEG4StereoHighProfileLevel4_2TransferSyntax,
-      UID_HEVCMainProfileLevel5_1TransferSyntax,
-      UID_HEVCMain10ProfileLevel5_1TransferSyntax,
-      UID_RLELosslessTransferSyntax,
-      UID_SMPTEST2110_20_UncompressedProgressiveActiveVideoTransferSyntax,
-      UID_SMPTEST2110_20_UncompressedInterlacedActiveVideoTransferSyntax,
-      UID_SMPTEST2110_30_PCMDigitalAudioTransferSyntax,
-      UID_RFC2557MIMEEncapsulationTransferSyntax,
-      UID_XMLEncodingTransferSyntax,
-      UID_PrivateGE_LEI_WithBigEndianPixelDataTransferSyntax
-};
-
-class ShutdownSentry
+class NetworkSentry
 {
 public:
-    ShutdownSentry() {}
-    ~ShutdownSentry() { OFStandard::shutdownNetwork(); }
+    NetworkSentry() { OFStandard::initializeNetwork(); }
+    ~NetworkSentry() { OFStandard::shutdownNetwork(); }
 };
 
 EchoDcmtk::EchoDcmtk(QObject *parent)
@@ -74,14 +26,11 @@ EchoDcmtk::~EchoDcmtk()
 
 void EchoDcmtk::echo()
 {
-    OFBool opt_abortAssociation = OFFalse;
     T_ASC_Network *net;
     T_ASC_Parameters *params;
     T_ASC_Association *assoc;
     OFString temp_str;
-    ShutdownSentry shutdown;
-
-    OFStandard::initializeNetwork();
+    NetworkSentry sentry;
 
     // always set the timeout values since the global default might be different
     dcmSocketSendTimeout.set(static_cast<Sint32>(socketTimeout()));
@@ -121,21 +70,13 @@ void EchoDcmtk::echo()
 
     /* Set the presentation contexts which will be negotiated */
     /* when the network connection will be established */
-    int presentationContextID = 1; /* odd byte value 1, 3, 5, .. 255 */
-    int numPresentationCtx = 1;
-    int numXferSyntaxes = 1;
-
-    for (int i = 0; i < numPresentationCtx; i++)
+    const char* transferSyntaxes[] = { UID_LittleEndianImplicitTransferSyntax }; // default transfer syntax
+    cond = ASC_addPresentationContext(params, 1, UID_VerificationSOPClass, transferSyntaxes, DIM_OF(transferSyntaxes));
+    if (cond.bad())
     {
-        cond = ASC_addPresentationContext(params, OFstatic_cast(T_ASC_PresentationContextID, presentationContextID),
-            UID_VerificationSOPClass, transferSyntaxes, OFstatic_cast(int, numXferSyntaxes));
-        presentationContextID += 2;
-        if (cond.bad())
-        {
-            DimseCondition::dump(temp_str, cond);
-            emit done(false, QString(temp_str.c_str()));
-            return;
-        }
+        DimseCondition::dump(temp_str, cond);
+        emit done(false, QString(temp_str.c_str()));
+        return;
     }
 
     /* dump presentation contexts if required */
@@ -189,28 +130,14 @@ void EchoDcmtk::echo()
     {
         emit done(true, "Echo successfully");
 
-        if (opt_abortAssociation)
+        /* release association */
+        qDebug() << "Releasing Association";
+        cond = ASC_releaseAssociation(assoc);
+        if (cond.bad())
         {
-            qDebug() << "Aborting Association";
-            cond = ASC_abortAssociation(assoc);
-            if (cond.bad())
-            {
-                DimseCondition::dump(temp_str, cond);
-                emit done(false, "Association Abort Failed: " + QString(temp_str.c_str()));
-                return;
-            }
-        }
-        else
-        {
-            /* release association */
-            qDebug() << "Releasing Association";
-            cond = ASC_releaseAssociation(assoc);
-            if (cond.bad())
-            {
-                DimseCondition::dump(temp_str, cond);
-                emit done(false, "Association Release Failed: " + QString(temp_str.c_str()));
-                return;
-            }
+            DimseCondition::dump(temp_str, cond);
+            emit done(false, "Association Release Failed: " + QString(temp_str.c_str()));
+            return;
         }
     }
     else if (cond == DUL_PEERREQUESTEDRELEASE)
